fd75f59a862274177b1e3320990867e5d6389144
Mateusz Kifner <29428687+mateusz-kifner@users.noreply.github.com>
2024-06-04 19:41:03 +0200

    added another gradient button

--- apps/nextjs/src/app/test/page.tsx
+++ apps/nextjs/src/app/test/page.tsx
@@ -1,4 +1,5 @@
-import { IconCheck } from "@tabler/icons-react";
+"use client";
+import { IconCheck, IconStarFilled } from "@tabler/icons-react";
 import { Button, buttonVariants } from "@tyfons-lab/ui-web/button";
 import GradientBorderContainer from "@tyfons-lab/ui-web/gradient-border-container";
 
@@ -11,14 +12,33 @@ function TestPage() {
       <GradientBorderContainer
         className={buttonVariants({
           variant: "outline",
-          className: "border-none hover:bg-transparent",
+          size: "icon",
+          className: "border-none hover:bg-transparent cursor-pointer",
         })}
         style={{ borderRadius: "calc(var(--radius) - 1px)" }}
         borderWidth="1px"
-        shadowWidth="0px"
+        gradient="linear-gradient(-25deg, rgba(255,0,0,1) 0%, rgba(255,154,0,1) 10%, rgba(208,222,33,1) 20%, rgba(79,220,74,1) 30%, rgba(63,218,216,1) 40%, rgba(47,201,226,1) 50%, rgba(28,127,238,1) 60%, rgba(95,21,242,1) 70%, rgba(186,12,248,1) 80%, rgba(251,7,217,1) 90%, rgba(255,0,0,1) 100% )"
+        onClick={() => {
+          console.log("click");
+        }}
       >
-        Test
         <IconCheck />
+      </GradientBorderContainer>
+      <GradientBorderContainer
+        className={buttonVariants({
+          variant: "outline",
+          size: "icon",
+          className: "border-none  cursor-pointer rounded-full",
+        })}
+        borderWidth="1px"
+        borderRadius="100%"
+        shadowWidth="0.25rem"
+        gradient="conic-gradient( #FFFFFFFF 0%, #FFEE00FF 11%, #FFFFFFFF 23%, #FFEE00FF 43%, #FFFFFFFF 57%, #FFEE00FF 72%, #FFFFFFFF 88%)"
+        onClick={() => {
+          console.log("click");
+        }}
+      >
+        <IconStarFilled className="fill-yellow-500" />
       </GradientBorderContainer>
     </div>
   );
--- packages/ui-web/src/gradient-border-container.tsx
+++ packages/ui-web/src/gradient-border-container.tsx
@@ -8,6 +8,8 @@ interface GradientBorderContainerProps extends ComponentProps<"div"> {
   borderRadius?: number | string;
   shadowBlur?: number | string;
   gradient?: string;
+  borderClassName?: string;
+  shadowClassName?: string;
 }
 
 const GradientBorderContainer = (props: GradientBorderContainerProps) => {
@@ -19,6 +21,8 @@ const GradientBorderContainer = (props: GradientBorderContainerProps) => {
     borderRadius = "1rem",
     shadowBlur = "10px",
     gradient = "conic-gradient(rgba(255,0,0,1) 0%, rgba(255,154,0,1) 10%, rgba(208,222,33,1) 20%, rgba(79,220,74,1) 30%, rgba(63,218,216,1) 40%, rgba(47,201,226,1) 50%, rgba(28,127,238,1) 60%, rgba(95,21,242,1) 70%, rgba(186,12,248,1) 80%, rgba(251,7,217,1) 90%, rgba(255,0,0,1) 100% )",
+    shadowClassName,
+    borderClassName,
     ...moreProps
   } = props;
 
@@ -26,20 +30,20 @@ const GradientBorderContainer = (props: GradientBorderContainerProps) => {
     <div {...moreProps} className={cn("relative", className)}>
       {shadowWidth !== 0 && shadowWidth !== "0px" && (
         <div
-          className="-z-10 absolute"
+          className={cn("-z-10 absolute", shadowClassName)}
           style={{
             background: gradient,
             left: `-${shadowWidth}`,
             right: `-${shadowWidth}`,
             top: `-${shadowWidth}`,
             bottom: `-${shadowWidth}`,
-            borderRadius: `calc(${shadowWidth}/2)`,
+            borderRadius: `calc(${borderRadius}/2)`,
             filter: `blur(${shadowBlur})`,
           }}
         />
       )}
       <div
-        className="-z-10 absolute"
+        className={cn("-z-10 absolute", borderClassName)}
         style={{
           background: gradient,
           left: `-${borderWidth}`,
