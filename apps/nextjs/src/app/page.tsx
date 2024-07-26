d5163590f23c644415f239ee5d566f0cc8ca0f36
Mateusz Kifner <29428687+mateusz-kifner@users.noreply.github.com>
2024-07-26 13:40:31 +0200

    test trpc

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,9 +1,29 @@
+"use client";
+import { api } from "@/trpc/react";
 import { Button } from "@tyfons-lab/ui-web/button";
 
-export default async function HomePage() {
+export default function HomePage() {
+  const { data } = api.post.all.useQuery();
+  const { mutateAsync } = api.post.create.useMutation();
+
   return (
     <>
-      <Button>test</Button>
+      <ul>
+        {data?.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+      <Button
+        onClick={() => {
+          mutateAsync({ title: "test", content: "test" })
+            .then(() => {})
+            .catch((e) => {
+              console.log(e);
+            });
+        }}
+      >
+        test
+      </Button>
     </>
   );
 }
