2e385e91f58b3cbafd80a3f6b939b65ed258c43b
Mateusz Kifner <29428687+mateusz-kifner@users.noreply.github.com>
2024-08-02 11:04:07 +0200

    init wrpc

--- apps/wss/src/events/test.ts
+++ apps/wss/src/events/test.ts
@@ -1,17 +1,28 @@
-export const testEvent = {
-  onSubscribe: async () => {
-    console.log("subscribed");
+import {
+  broadcastMessage,
+  peerMessage,
+  router,
+  serverMessage,
+} from "@/wrpc/initWRPC";
+
+export const testEvent = router(
+  {
+    helloServer: serverMessage.emit(async () => {
+      console.log("emit serverMessage");
+    }),
+    helloPeer: peerMessage.emit(async (userId: string) => {
+      console.log("emit peerMessage", userId);
+    }),
+    helloBroadcast: broadcastMessage.emit(async () => {
+      console.log("emit broadcastMessage");
+    }),
   },
-  onUnsubscribe: async () => {
-    console.log("unsubscribed");
+  {
+    onSubscribe: async () => {
+      console.log("subscribed");
+    },
+    onUnsubscribe: async () => {
+      console.log("unsubscribed");
+    },
   },
-  emit: async () => {
-    console.log("emit");
-  },
-  // getSession: publicProcedure.query(({ ctx }) => {
-  //   return ctx.session;
-  // }),
-  // getSecretMessage: protectedProcedure.query(() => {
-  //   return "you can see this secret message!";
-  // }),
-};
+);
---
+++ apps/wss/src/wrpc/initWRPC.ts
@@ -0,0 +1,125 @@
+interface RouterActionsType {
+  onSubscribe?: () => void;
+  onUnsubscribe?: () => void;
+}
+
+interface RouterMessagesType {
+  [key: string]: Promise<void>;
+}
+
+export function router(
+  messageHandlers: RouterMessagesType,
+  lifetimeActions?: RouterActionsType,
+) {}
+
+export const serverMessage = {
+  emit: async (action: () => Promise<void>) => {},
+};
+
+export const peerMessage = {
+  emit: async (action: (userId: string) => Promise<void>) => {},
+};
+
+export const broadcastMessage = {
+  emit: async (action: () => Promise<void>) => {},
+};
+
+// WRPC starts here
+// based on TRPC
+
+type inferErrorFormatterShape<TType> = TType extends ErrorFormatter<
+  any,
+  infer TShape
+>
+  ? TShape
+  : DefaultErrorShape;
+interface RuntimeConfigOptions<TContext extends object, TMeta extends object>
+  extends Partial<
+    Omit<
+      RootConfig<{
+        ctx: TContext;
+        meta: TMeta;
+        errorShape: any;
+        transformer: any;
+      }>,
+      "$types" | "transformer"
+    >
+  > {
+  transformer?: DataTransformerOptions;
+}
+
+type ContextCallback = (...args: any[]) => object | Promise<object>;
+
+class WRPCBuilder<TContext extends object, TMeta extends object> {
+  context<TNewContext extends object | ContextCallback>() {
+    return new WRPCBuilder<
+      TNewContext extends ContextCallback ? Unwrap<TNewContext> : TNewContext,
+      TMeta
+    >();
+  }
+
+  meta<TNewMeta extends object>() {
+    return new WRPCBuilder<TContext, TNewMeta>();
+  }
+
+  create<TOptions extends RuntimeConfigOptions<TContext, TMeta>>(
+    opts?:
+      | ValidateShape<TOptions, RuntimeConfigOptions<TContext, TMeta>>
+      | undefined,
+  ) {
+    type $Root = CreateRootTypes<{
+      ctx: TContext;
+      meta: TMeta;
+      errorShape: undefined extends TOptions["errorFormatter"]
+        ? DefaultErrorShape
+        : inferErrorFormatterShape<TOptions["errorFormatter"]>;
+      transformer: undefined extends TOptions["transformer"] ? false : true;
+    }>;
+
+    const config: RootConfig<$Root> = {
+      transformer: getDataTransformer(opts?.transformer ?? defaultTransformer),
+      isDev:
+        opts?.isDev ??
+        // eslint-disable-next-line @typescript-eslint/dot-notation
+        globalThis.process?.env["NODE_ENV"] !== "production",
+      allowOutsideOfServer: opts?.allowOutsideOfServer ?? false,
+      errorFormatter: opts?.errorFormatter ?? defaultFormatter,
+      isServer: opts?.isServer ?? isServerDefault,
+      /**
+       * These are just types, they can't be used at runtime
+       * @internal
+       */
+      $types: null as any,
+      experimental: opts?.experimental ?? {},
+    };
+
+    {
+      // Server check
+      const isServer: boolean = opts?.isServer ?? isServerDefault;
+
+      if (!isServer && opts?.allowOutsideOfServer !== true) {
+        throw new Error(
+          `You're trying to use @wrpc/server in a non-server environment. This is not supported by default.`,
+        );
+      }
+    }
+    return {
+      _config: config,
+
+      procedure: createBuilder<$Root["ctx"], $Root["meta"]>({
+        meta: opts?.defaultMeta,
+      }),
+
+      middleware: createMiddlewareFactory<$Root["ctx"], $Root["meta"]>(),
+
+      router: createRouterFactory<$Root>(config),
+
+      mergeRouters,
+
+      createCallerFactory: createCallerFactory<$Root>(),
+    };
+  }
+}
+
+export const initWRPC = new WRPCBuilder();
+export type { WRPCBuilder };
